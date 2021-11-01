3b5b99951364aedf10a5027459e25d6375a05599
vLuuk <luckyluuk.6@gmail.com>
2021-11-01 14:33:15 +0100

    feat: add cars page and create car form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employee/employees/employees.component';
 import { ClientsComponent } from './client/clients/clients.component';
+import { CarsComponent } from './car/cars/cars.component';
 
 const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
-  { path: 'clients', component: ClientsComponent }
+  { path: 'clients', component: ClientsComponent },
+  { path: 'cars', component: CarsComponent }
 ];
 
 @NgModule({
