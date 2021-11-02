730e39541be1c01778f07cb2b9f78476d132726e
vLuuk <luckyluuk.6@gmail.com>
2021-11-02 11:17:29 +0100

    fix: fix login page and project structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './shared/material/material.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
