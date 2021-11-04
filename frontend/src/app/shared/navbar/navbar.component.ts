ba347f2adf5498cb69e64ad81cc1d4af5a8aed82
vLuuk <luckyluuk.6@gmail.com>
2021-11-04 14:08:42 +0100

    fix: make navbar responsive and animated

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { onSideNavChange, animateText } from 'src/app/animations/animations';
+import { SidenavService } from 'src/app/services/navbar.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [onSideNavChange, animateText]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public sideNavState: boolean = true;
+  public linkText: boolean = true;
+
+  constructor(private _sidenavService: SidenavService) { }
 
   ngOnInit(): void {
+  }
+
+  onSidenavToggle() {
+    this.sideNavState = !this.sideNavState;
+
+    setTimeout(() => {
+      this.linkText = this.sideNavState;
+    }, 200);
+    this._sidenavService.sideNavState$.next(this.sideNavState);
   }
 
 }
