21609ab8f43740dc044200f58eb9ba8cd9473851
Riad Joul <joulriad@gmail.com>
2021-12-07 15:03:09 +0100

    connected to the API

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import axios from 'axios';
 
-const AUTH_API = 'http://127.0.0.1:8000/api/';
+const AUTH_API = 'http://localhost:8000/api/';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,8 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-
-  login(email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'login', {
+  login(email: string, password: string){
+    return axios.post(AUTH_API + 'login',{
       email,
       password
     });
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+const API_URL = 'http://localhost:8000/api/';
 
+const Token = localStorage.getItem('session');
+;
 @Injectable({
   providedIn: 'root'
 })
@@ -15,8 +18,8 @@ export class UserService {
     return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
 
-  user() : Observable<any> {
-    return this.http.get(API_URL + 'user');
+  user(){
+    return axios.get(API_URL + 'user',{ headers: {"Authorization" : `Bearer ${Token}`} });
   }
 
 }
